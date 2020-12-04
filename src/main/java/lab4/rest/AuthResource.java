db7dec69e3ef7f2db811a8af6284a890c756c0b1
notgurev <notgurev@gmail.com>
2020-12-05 00:31:38 +0300

    Login and register working (AuthResource methods done)

--- src/main/java/lab4/rest/AuthResource.java
+++ src/main/java/lab4/rest/AuthResource.java
@@ -1,20 +1,43 @@
 package lab4.rest;
 
+import lab4.rest.json.Credentials;
+import lab4.services.auth.AuthResult;
+import lab4.services.auth.AuthService;
+
+import javax.ejb.EJB;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/auth")
 public class AuthResource {
+    @EJB
+    private AuthService authService;
+
     @POST
     @Path("/login")
-    public Response login() {
-        return Response.ok().build();
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response login(@NotNull Credentials credentials) {
+        AuthResult result = authService.login(credentials.getUsername(), credentials.getPassword());
+        if (result.isSuccessful()) {
+            return Response.ok(result.getToken()).build();
+        } else {
+            return Response.status(Response.Status.FORBIDDEN).entity(result.getErrorMessage()).build();
+        }
     }
 
     @POST
     @Path("register")
-    public Response register() {
-        return Response.ok().build();
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response register(@NotNull Credentials credentials) {
+        AuthResult result = authService.register(credentials.getUsername(), credentials.getPassword());
+        if (result.isSuccessful()) {
+            return Response.ok(result.getToken()).build();
+        } else {
+            return Response.status(Response.Status.FORBIDDEN).entity(result.getErrorMessage()).build();
+        }
     }
 }
