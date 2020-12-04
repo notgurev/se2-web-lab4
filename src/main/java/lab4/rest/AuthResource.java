f15e4c2022c7ae052e0afb3c660c1652dad8ee8b
notgurev <notgurev@gmail.com>
2020-12-04 20:21:08 +0300

    LoginResource renamed to AuthResource

--- src/main/java/lab4/rest/AuthResource.java
+++ src/main/java/lab4/rest/AuthResource.java
@@ -1,19 +1,11 @@
 package lab4.rest;
 
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
-@Path("/login")
-public class LoginResource {
-    // todo no idea what to do (yet)
-
-    @GET
-    public Response placeholder() {
-        return Response.ok().build();
-    }
-
+@Path("/auth")
+public class AuthResource {
     @POST
     @Path("/login")
     public Response login() {
--- src/main/java/lab4/rest/MainResource.java
+++ src/main/java/lab4/rest/MainResource.java
@@ -7,17 +7,15 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/main")
 public class MainResource {
-    // todo @GET main page? how the fuck does that work
-
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("hits")
-    public JsonObject getHitsData() { // todo params: user identifier?
+    public JsonObject getHitsData() { // todo params: token
         return Json.createObjectBuilder().add("first", 1).add("second", 2).build();
     }
 
     @POST
-    @Produces(MediaType.APPLICATION_JSON) // todo get list of new hits?
+    @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     @Path("hits")
     public String addHit() { // todo params: user identifier?
