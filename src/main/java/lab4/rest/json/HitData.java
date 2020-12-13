c74eaf29077aca42fd3a7d4f3cdac3e55989ca03
notgurev <notgurev@gmail.com>
2020-12-13 06:45:27 +0300

    @InArray validation for X, R

--- src/main/java/lab4/rest/json/HitData.java
+++ src/main/java/lab4/rest/json/HitData.java
@@ -1,5 +1,6 @@
 package lab4.rest.json;
 
+import lab4.utils.validation.InArray;
 import lombok.Data;
 
 import javax.validation.constraints.DecimalMax;
@@ -10,22 +11,25 @@ import java.math.BigDecimal;
 
 @Data
 public class HitData {
-    //  Координата X - Listbox {'-5','-4','-3','-2','-1','0','1','2','3'}
-    //  Координата Y - Slider  (-5 ... 5)
-    //  Радиус R - Listbox {'-5','-4','-3','-2','-1','0','1','2','3'}
-    private static final int[] ALLOWED_X = {-5, -4, -3, -2, -1, 0, 1, 2, 3};
-    private static final int[] ALLOWED_R = {-5, -4, -3, -2, -1, 0, 1, 2, 3};
-    private static final float MIN_Y = -5;
-    private static final float MAX_Y = 5;
-
-    //    @InArray(array = {-5, -4, -3, -2, -1, 0, 1, 2, 3}) todo
+    /*
+      Координата X : Listbox {'-5','-4','-3','-2','-1','0','1','2','3'}
+      Координата Y : Slider  (-5 ... 5)
+          Радиус R : Listbox {'-5','-4','-3','-2','-1','0','1','2','3'}
+    */
+    @InArray(
+            message = "Allowed X values are {-5, -4, -3, -2, -1, 0, 1, 2, 3}",
+            array = {-5, -4, -3, -2, -1, 0, 1, 2, 3}
+    )
     @NotNull(message = "X must not be null")
     private int x;
     @DecimalMax(value = "5", inclusive = false, message = "Y must be in range (-5 ... 5)")
     @DecimalMin(value = "-5", inclusive = false, message = "Y must be in range (-5 ... 5)")
     @NotNull(message = "Y must not be null")
     private BigDecimal y;
-    //    @InArray(array = {-5, -4, -3, -2, -1, 0, 1, 2, 3}) todo
+    @InArray(
+            message = "Allowed Y values are {-5, -4, -3, -2, -1, 0, 1, 2, 3}",
+            array = {-5, -4, -3, -2, -1, 0, 1, 2, 3}
+    )
     @Max(1)
     @NotNull(message = "R must not be null")
     private int r;
