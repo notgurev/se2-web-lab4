33b7b765d414c7440b8ce4646f42034df7bebfbb
notgurev <notgurev@gmail.com>
2020-12-13 04:05:54 +0300

    Authorization with TokenFilter (using NameBinding), cleaner addHit() and getHitsData(); removed specifying user in URI

--- src/main/java/lab4/rest/MainResource.java
+++ src/main/java/lab4/rest/MainResource.java
@@ -1,8 +1,8 @@
 package lab4.rest;
 
 import lab4.beans.Hit;
+import lab4.rest.filters.authorization.Authorized;
 import lab4.rest.json.HitData;
-import lab4.services.auth.AuthService;
 import lab4.services.hits.HitService;
 
 import javax.ejb.EJB;
@@ -11,64 +11,29 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.Optional;
-
-import static javax.ws.rs.core.HttpHeaders.AUTHORIZATION;
-import static javax.ws.rs.core.Response.Status.FORBIDDEN;
 
 @Path("/hits")
 public class MainResource {
-    private static final String AUTHENTICATION_SCHEME = "Bearer";
-    @EJB
-    private AuthService authService;
-
     // todo clear()
     @EJB
     private HitService hitService;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("{user}")
-    public Response getHitsData(@PathParam("user") String userFromPath, @Context HttpHeaders headers) {
-        String token = getTokenFromHeaders(headers);
-        Response response = checkAccess(token, userFromPath);
-        if (response != null) return response;
-
-        // since token is valid and usernames match we can assume that user with userFromPath exists
-        return Response.ok(hitService.getAllJSON(userFromPath)).build();
+    @Authorized // ensures that user exists
+    public Response getHitsData(@Context HttpHeaders headers) {
+        // since token is valid we can assume that user from token exists
+        String username = headers.getHeaderString("username");
+        return Response.ok(hitService.getAllJSON(username)).build();
     }
 
     @POST
-    @Path("{user}")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response addHit(@PathParam("user") String userFromPath, @Context HttpHeaders headers, HitData hitData) {
-        String token = getTokenFromHeaders(headers);
-        Response response = checkAccess(token, userFromPath);
-        if (response != null) return response;
-
-        // since token is valid and usernames match we can assume that user with userFromPath exists
-        hitService.add(new Hit(hitData.getX(), hitData.getY(), hitData.getR()), userFromPath);
-
-        return Response.ok(String.format("hit added (owner is %s)", userFromPath)).build();
-    }
-
-    private String getTokenFromHeaders(HttpHeaders headers) {
-        return headers.getHeaderString(AUTHORIZATION).substring(AUTHENTICATION_SCHEME.length()).trim();
-    }
-
-    private Response checkAccess(String token, String userFromPath) {
-        Optional<String> usernameByToken = authService.getUsernameByToken(token);
-
-        // validate token
-        if (!usernameByToken.isPresent()) {
-            return Response.status(FORBIDDEN).entity("invalid token").build();
-        }
-
-        // check if users match
-        if (!usernameByToken.get().equals(userFromPath)) {
-            return Response.status(FORBIDDEN).entity("you dont have access to this users data").build();
-        }
-
-        return null;
+    @Authorized // ensures that user exists
+    public Response addHit(@Context HttpHeaders headers, HitData hitData) {
+        // since token is valid we can assume that user from token exists
+        String username = headers.getHeaderString("username");
+        hitService.add(new Hit(hitData.getX(), hitData.getY(), hitData.getR()), username);
+        return Response.ok(String.format("hit added (owner is %s)", username)).build();
     }
 }
---
+++ src/main/java/lab4/rest/filters/authorization/TokenFilter.java
@@ -0,0 +1,61 @@
+package lab4.rest.filters.authorization;
+
+import lab4.services.auth.AuthService;
+
+import javax.annotation.Priority;
+import javax.ejb.EJB;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.Priorities;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+import java.util.Optional;
+
+import static javax.ws.rs.core.HttpHeaders.AUTHORIZATION;
+import static javax.ws.rs.core.Response.Status.FORBIDDEN;
+
+@Authorized
+@Priority(Priorities.AUTHENTICATION)
+@Provider
+public class TokenFilter implements ContainerRequestFilter {
+    private static final String AUTHENTICATION_SCHEME = "Bearer";
+
+    @EJB
+    private AuthService authService;
+
+    @Override
+    public void filter(ContainerRequestContext containerRequestContext) {
+        // get token from headers
+        Optional<String> token = getTokenFromContext(containerRequestContext);
+
+        // check if token is present
+        if (!token.isPresent()) {
+            containerRequestContext.abortWith(
+                    Response.status(FORBIDDEN).entity("missing authorization token").build()
+            );
+            return;
+        }
+
+        // try to get username from token
+        // validates token and returns username
+        Optional<String> usernameByToken = authService.getUsernameByToken(token.get());
+
+        // if token missing or bad, respond with FORBIDDEN
+        if (!usernameByToken.isPresent()) {
+            containerRequestContext.abortWith(
+                    Response.status(FORBIDDEN).entity("invalid token").build()
+            );
+            return;
+        }
+
+        // put username to headers for further use
+        containerRequestContext.getHeaders().add("username", usernameByToken.get());
+    }
+
+    private Optional<String> getTokenFromContext(@NotNull ContainerRequestContext containerRequestContext) {
+        String authHeaderString = containerRequestContext.getHeaderString(AUTHORIZATION);
+        return authHeaderString == null ?
+                Optional.empty() : Optional.of(authHeaderString.substring(AUTHENTICATION_SCHEME.length()).trim());
+    }
+}
