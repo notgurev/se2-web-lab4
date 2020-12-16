66a8b347a624a2b0113dbd7821b80c8c3508e413
notgurev <notgurev@gmail.com>
2020-12-16 04:42:21 +0300

    @angular/material

--- src/main/angular/lab-app/src/app/app.module.ts
+++ src/main/angular/lab-app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {MainPageComponent} from './main-page/main-page.component';
 import {AuthPageComponent} from './auth-page/auth-page.component';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import {AuthPageComponent} from './auth-page/auth-page.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    // BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
