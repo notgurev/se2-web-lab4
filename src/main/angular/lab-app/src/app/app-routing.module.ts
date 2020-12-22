34bcff01f3ecc9648481638d82eb1e5f3823496b
notgurev <notgurev@gmail.com>
2020-12-22 10:25:16 +0300

    AuthGuard

--- src/main/angular/lab-app/src/app/app-routing.module.ts
+++ src/main/angular/lab-app/src/app/app-routing.module.ts
@@ -2,11 +2,22 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {AuthPageComponent} from "./auth-page/auth-page.component";
 import {MainPageComponent} from "./main-page/main-page.component";
+import {AuthGuardService} from "./auth-guard.service";
 
 const routes: Routes = [
-  {path: 'auth', component: AuthPageComponent},
-  {path: '', component: MainPageComponent},
-  {path: '**', redirectTo: ''}
+  {
+    path: 'auth',
+    component: AuthPageComponent
+  },
+  {
+    path: '',
+    component: MainPageComponent,
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: '**',
+    redirectTo: '' // todo not working
+  }
 ];
 
 @NgModule({
---
+++ src/main/angular/lab-app/src/app/auth-guard.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from "@angular/router";
+import {AuthService} from "./auth.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService implements CanActivate {
+
+  constructor(private auth: AuthService, private router: Router) {
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (!this.auth.loggedIn) {
+      this.router.navigate(['auth']); // todo should leave a message or something
+      return false;
+    }
+    return true;
+  }
+}
