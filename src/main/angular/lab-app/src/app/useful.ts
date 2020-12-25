55b195f5cfe341693b48297f8ed97dcadfcb47c7
notgurev <notgurev@gmail.com>
2020-12-25 04:58:41 +0300

    canvas.js -> .ts

--- src/main/angular/lab-app/src/app/useful.ts
+++ src/main/angular/lab-app/src/app/useful.ts
@@ -3,3 +3,11 @@ import {Router} from "@angular/router";
 export function route(url: string, router: Router) {
   router.navigateByUrl(url).then(() => console.log('Navigated to ' + url))
 }
+
+export function canvasRelativeX(event: MouseEvent, canvasContainer: HTMLElement) {
+  return event.pageX - canvasContainer.offsetLeft;
+}
+
+export function canvasRelativeY(event: MouseEvent, canvasContainer: HTMLElement) {
+  return event.pageY - canvasContainer.offsetTop;
+}
