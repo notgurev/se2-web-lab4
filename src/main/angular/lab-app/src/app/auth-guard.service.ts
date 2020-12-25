8506fa51cd9f91158e5a99add4fd0c6187ba7cc0
notgurev <notgurev@gmail.com>
2020-12-25 19:17:11 +0300

    minor change

--- src/main/angular/lab-app/src/app/auth-guard.service.ts
+++ src/main/angular/lab-app/src/app/auth-guard.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from "@angular/router";
 import {AuthService} from "./auth.service";
+import {route} from "./useful";
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,9 @@ export class AuthGuardService implements CanActivate {
   constructor(private auth: AuthService, private router: Router) {
   }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+  canActivate(routeSnap: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     if (!this.auth.loggedIn) {
-      this.router.navigate(['auth']); // todo should leave a message or something
+      route('auth', this.router) // todo should leave a message or something
       return false;
     }
     return true;
