29b8189d1185e05b7332fdf445b424ea7f91eca4
notgurev <notgurev@gmail.com>
2020-12-25 04:14:51 +0300

    empty method in PointService

--- src/main/angular/lab-app/src/app/point.service.ts
+++ src/main/angular/lab-app/src/app/point.service.ts
@@ -1,16 +1,19 @@
 import {Injectable} from '@angular/core';
-
-export interface Hit {
-  x: number,
-  y: number,
-  r: number,
-  result: boolean
-}
+import {AuthService} from "./auth.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PointService {
-  constructor() {
+  constructor(private authService: AuthService) {
+  }
+
+  // fake method for dev
+  submitHit(x: number, y: number, radius: number) {
+    console.log(`Submitted point with x = ${x}, y = ${y}, r = ${radius}`)
+  }
+
+  getHits() {
+
   }
 }
