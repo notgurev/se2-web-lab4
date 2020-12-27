6bdc16bfd6cb153c07e78df998cacf8e71dfc7f1
notgurev <notgurev@gmail.com>
2020-12-28 02:08:45 +0300

    [backend] Credentials constraints fix (@min/max -> @Size)

--- src/main/java/lab4/rest/json/Credentials.java
+++ src/main/java/lab4/rest/json/Credentials.java
@@ -2,20 +2,19 @@ package lab4.rest.json;
 
 import lombok.Data;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 public class Credentials {
     @NotNull(message = "USERNAME_MUST_NOT_BE_NULL_OR_EMPTY")
     @NotEmpty(message = "USERNAME_MUST_NOT_BE_NULL_OR_EMPTY")
-    @Min(value = 4, message = "USERNAME_TOO_SHORT")
-    @Max(value = 15, message = "USERNAME_TOO_LONG")
+    @Size(min = 4, message = "USERNAME_TOO_SHORT")
+    @Size(max = 15, message = "USERNAME_TOO_LONG")
     private String username;
     @NotNull(message = "PASSWORD_MUST_NOT_BE_NULL_OR_EMPTY")
     @NotEmpty(message = "PASSWORD_MUST_NOT_BE_NULL_OR_EMPTY")
-    @Min(value = 4, message = "PASSWORD_TOO_SHORT")
+    @Size(min = 4, message = "PASSWORD_TOO_SHORT")
     private String password;
 }
